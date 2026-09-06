358e4f903c39315f1781971b69fd9c038b4a78bf
EffessDev <faseeh1080@gmail.com>
2026-09-06 14:05:15 +0530

    fix bg shift mobile

--- app/layout.tsx
+++ app/layout.tsx
@@ -59,7 +59,7 @@ export default function RootLayout({
         {/* Fixed background div */}
         <div
           aria-hidden="true"
-          className="fixed inset-0 -z-10"
+          className="fixed inset-0 -z-10 min-h-dvh"
           style={{
             backgroundImage: `url(${backgroundImage.src})`,
             backgroundPosition: "center",
