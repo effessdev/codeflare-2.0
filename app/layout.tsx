950d7c9db7c1b5776cb48d196a8682724e680c36
EffessDev <faseeh1080@gmail.com>
2026-09-06 11:05:07 +0530

    add bg

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import {
   Playfair_Display,
 } from "next/font/google"
 
+import backgroundImage from "./assets/luminas-art-waterfall.jpg"
 import "./globals.css"
 import { cn } from "@/lib/utils"
 
@@ -43,7 +44,18 @@ export default function RootLayout({
         playfairDisplayHeading.variable
       )}
     >
-      <body>{children}</body>
+      <body
+        className="min-h-screen bg-background text-foreground"
+        style={{
+          backgroundImage: `url(${backgroundImage.src})`,
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+          backgroundAttachment: "fixed",
+        }}
+      >
+        {children}
+      </body>
     </html>
   )
 }
